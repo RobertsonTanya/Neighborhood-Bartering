31bce69fb4131386548aa3de24953c702dca7d63
Christina pilat <cpmoneyy@gmail.com>
2022-07-23 17:10:49 -0400

    Start working on MyItemDisplay

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const port = process.env.MY_PORT;
 
 app.use(cookieParser());
 app.use(cors({
-    origin: `http://localhost:${port}`,
+    origin: `http://localhost:3000`,
     credentials: true
 }));
 app.use(express.json());
