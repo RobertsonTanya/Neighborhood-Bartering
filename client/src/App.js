ec17fcba1cafdd29aa5a78c29ad692104fd0fdfb
Christina pilat <cpmoneyy@gmail.com>
2022-07-23 18:19:14 -0400

    Re-create create item form replacement

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CreateItemForm from './components/CreateItemForm';
 import Dashboard from './components/Dashboard';
 import Requests from './components/RequestPage';
 
@@ -10,12 +11,13 @@ function App() {
 
   return (
     <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Dashboard />} default />
-            <Route path="/requests" element={<Requests />} default />
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />} default />
+          <Route path="/requests" element={<Requests />} default />
+          <Route path="/create" element={<CreateItemForm />} default />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
