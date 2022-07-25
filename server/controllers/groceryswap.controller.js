7db355cd0d60f618d00ad73199573eeaee122f45
Venus Quan <vxq5000@gmail.com>
2022-07-25 12:47:08 -0700

    added one to many- working

--- server/controllers/groceryswap.controller.js
+++ server/controllers/groceryswap.controller.js
@@ -3,79 +3,87 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
 module.exports = {
-  createNewItem: (req, res) => {
-    const newGroceryObject = new GrocerySwap(req.body);
-
+  createNewItem: async (req, res) => {
+    const {body}= req;
+    let newGroceryObject = new GrocerySwap(body);
+    newGroceryObject.createdBy = req.decodedJWT.payload.id;
     const decodedJWT = jwt.decode(req.cookies.usertoken, {
-      complete: true
-    })
-    newGroceryObject.createdBy = decodedJWT.payload.id;
+      complete: true})
+    try{
+      newGroceryObject= await newGroceryObject.save();
+      res.json(newGroceryObject);
+      return;
+    }catch (error) {
+      console.log('error', error);
+      res.status(400).json(error);
+      return;
+    }
+    },
 
-    newGroceryObject.save()
-      .then((newItem) => {
-        console.log(newItem);
-        res.json(newItem);
+  findAllItems: async (req, res) => {
+    try {
+      const allItems= await GrocerySwap.find()
+      .populate({
+        path: 'comments',
+        model:'Comment',
+        populate: {
+          path:'user',
+          model: 'User',
+        },
       })
-      .catch((err) => res.status(400).json({ err }));
+      .exec();
+      console.log(allItems);
+      res.json(allItems);
+    }catch (error) {
+      console.log('err block');
+      res.status(400).json(error);
+    }
   },
-
-  findAllItems: (req, res) => {
-    GrocerySwap.find()
-      .populate("createdBy", "username email")
-      .then((allItems) => {
-        console.log(allItems);
-        res.json(allItems);
+  
+  getOneItem: (req, res) => {
+    GrocerySwap.findOne({ _id: req.params.id })
+      .then((oneItem) => {
+        console.log(oneItem);
+        res.json(oneItem);
       })
       .catch((err) => {
-        console.log("error finding items");
+        console.log("error fnding item");
         res.status(400).json(err);
       });
   },
-  
-    getOneItem: (req, res) => {
-      GrocerySwap.findOne({ _id: req.params.id })
-        .then((oneItem) => {
-          console.log(oneItem);
-          res.json(oneItem);
+
+
+  findAllItemsByUser: (req, res) => {
+    if(req.jwtpayload.username !== req.params.username){
+      console.log('not the user')
+
+      User.findOne({username: req.params.username})
+        .then((userNotLoggedIn)=>{
+          GrocerySwap.find({createdBy: userNotLoggedIn._id})
+            .populate("createdBy", "username")
+            .then((allItemsFromUser)=>{
+              console.log(allItemsFromUser);
+              res.json(allItemsFromUser);
+            })
+            .catch((err)=>{
+              console.log(err);
+              res.status(400).json();
+            })
         })
-        .catch((err) => {
-          console.log("error fnding item");
-          res.status(400).json(err);
-        });
-    },
-
-
-    findAllItemsByUser: (req, res) => {
-      if(req.jwtpayload.username !== req.params.username){
-        console.log('not the user')
-
-        User.findOne({username: req.params.username})
-          .then((userNotLoggedIn)=>{
-            GrocerySwap.find({createdBy: userNotLoggedIn._id})
-              .populate("createdBy", "username")
-              .then((allItemsFromUser)=>{
-                console.log(allItemsFromUser);
-                res.json(allItemsFromUser);
-              })
-              .catch((err)=>{
-                console.log(err);
-                res.status(400).json();
-              })
-          })
-      } else {
-        console.log('current user');
-        GrocerySwap.find({createdBy: req.jwtpayload.id})
-          .populate("createdBy", "username")
-          .then((allItemsFromLoggedInUser)=>{
-            console.log(allItemsFromLoggedInUser);
-            res.json(allItemsFromLoggedInUser);
-          })
-          .catch((err)=>{
-            console.log(err);
-            res.status(400).json();
-          })
-      }
-    },
+    } else {
+      console.log('current user');
+      GrocerySwap.find({createdBy: req.jwtpayload.id})
+        .populate("createdBy", "username")
+        .then((allItemsFromLoggedInUser)=>{
+          console.log(allItemsFromLoggedInUser);
+          res.json(allItemsFromLoggedInUser);
+        })
+        .catch((err)=>{
+          console.log(err);
+          res.status(400).json();
+        })
+    }
+  },
 
   updateItem: (req, res) => {
     GrocerySwap.findOneAndUpdate({ _id: req.params.id }, req.body, {
