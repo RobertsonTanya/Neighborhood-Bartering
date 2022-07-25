150abf983826f38010acd8433cc8e45ed2beae63
RobertsonTanya <101999308+RobertsonTanya@users.noreply.github.com>
2022-07-25 14:39:12 -0400

    broken....dashboard populates empty because useEffect runs 2x

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,16 +1,64 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import Post from "./Post";
-import styles from "../styles/dashboard.module.css";
 import Header from "./Header";
 
+import styles from "../styles/dashboard.module.css";
+
+
+
 function Dashboard() {
+  const [user, setUser] = useState({});
+  const [items, setItems] = useState([{}]);
+
+  useEffect(()=>{
+    axios.get("http://localhost:8000/api/users",
+    {
+      withCredentials: true
+    })
+    .then((res)=>{
+      console.log(res.data);
+      setUser(res.data);
+    })
+    .then(()=>{
+      if(Object.keys(user).length){
+        axios.get(`/api/GrocerySwap/myItems/${user.username}`)
+        .then((res)=>{
+          console.log(res.data);
+          setItems(res.data);
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+      } else {
+        axios.get("http://localhost:8000/api/GrocerySwap/allItems")
+        .then((res)=>{
+          console.log(res.data);
+          setItems(res.data);
+        })
+        .catch((err)=>{
+          console.log(err);
+        })
+      }
+    })
+    .catch((err)=>{
+      console.log(err);
+      setUser({});
+    })
+    console.log('I fire once');
+  }, []);
+
 
   return (
     <div className={`container ${styles.container}`}>
       <Header showLoginBtn={true}/>
       <div className={styles.dashboard}>
-        <Post /><Post /><Post />
-        <Post /><Post /><Post />
+        {items ?
+          items.map((item, index)=>{
+            <Post key={index} item={item} />
+          })
+        : null}
       </div>
     </div>
   )
--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -2,17 +2,19 @@ import React from 'react'
 import styles from '../styles/post.module.css';
 
 
-function Post() {
+function Post(props) {
+  const { item } = props;
+
   return (
     <div className={styles.box}>
-      <h2>Name of Item</h2>
-      <p className={styles.dateTime}>Date and Time Posted</p>
+      <h2>{item.itemName}</h2>
+      <p className={styles.dateTime}>{item.createdAt}</p>
       <div className={styles.split}>
-        <img src="https://images.unsplash.com/photo-1619566636858-adf3ef46400b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="fruits" />
-        <p>Short Description</p>
+        <img src={item.imgUrl} alt={item.name} />
+        <p>{item.description}</p>
       </div>
       <div className={styles.split}>
-        <p>Suggested Item</p>
+        <p>{item.sugItem}</p>
         <button>Request Trade</button>
       </div>
       <input type="checkbox" name="" id="alternate" />
