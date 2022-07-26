65b8a81a4d92381cc60da4e18f376a28e7d0cabf
RobertsonTanya <101999308+RobertsonTanya@users.noreply.github.com>
2022-07-26 16:50:29 -0400

    add user to post and style

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -4,13 +4,15 @@ import styles from '../styles/post.module.css';
 
 
 function Post(props) {
-  const { item } = props;
+  const { item, user } = props;
   const date = item.createdAt ? item.createdAt.slice(0, 10) : null;
+
 
   return (
     <div className={styles.box}>
       <h2>{item.itemName}</h2>
-      <p className={styles.dateTime}>Posted: {date}</p>
+      <p className={styles.dateUser}><span>Posted by: </span>{user.userLoggedIn}</p>
+      <p className={styles.dateUser}><span>Posted: </span>{date}</p>
       <div className={styles.split}>
         <span className={styles.imgContainer}>
           <img className={styles.img} src={item.imgUrl} alt={item.name} />
