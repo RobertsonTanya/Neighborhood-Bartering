13bd4c0792806c026cf0daca56c8eb098c2618ef
Venus Quan <vxq5000@gmail.com>
2022-07-27 09:27:23 -0700

    added comment to post

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,29 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+
 import Post from "./Post";
 import Header from "./Header";
+
 import styles from "../styles/dashboard.module.css";
+
 
 
 function Dashboard(props) {
   const { user, setUser } = props;
   const [items, setItems] = useState([{}]);
-  const [commentText, setCommentText]= useState("");
 
   useEffect(()=>{
-    async function getData(){
-      try{
-        const response= await axios.get("http://localhost:8000/api/GrocerySwap/allItems", {
-          withCredentials: true}
-          );
-        console.log(response.data);
-          setItems(response.data);
-      } catch(error){
-        console.log(error);
-      }
-    };
-  getData();
-}, []);
+      axios.get("http://localhost:8000/api/GrocerySwap/allItems")
+      .then((res)=>{
+        console.log(res.data);
+        setItems(res.data);
+      })
+      .catch((err)=>{
+        console.log(err.response);
+      })
+  }, []);
 
 
   return (
