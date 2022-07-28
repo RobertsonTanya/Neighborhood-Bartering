15c2530b97a8cb08fec27dc5e993c3b22d410d7d
Christina pilat <cpmoneyy@gmail.com>
2022-07-28 11:55:14 -0400

    nav burger css almost there

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import logo from "../images/logo.png";
 
 import styles from "../styles/header.module.css";
+import "../styles/Nav.css";
 
 const Header = (props) => {
   const { showLoginBtn, user, setUser } = props;
@@ -35,45 +36,49 @@ const Header = (props) => {
   // }
 
   return (
-    <div className={styles.navigation}>
+    <div className={styles.topNav}>
       <div className={styles.navLeft}>
         <Link to="/">
           <img className={styles.logo} src={logo} alt="logo" />
         </Link>
       </div>
       <div className={styles.navRight}>
+        <input id="menu-toggle" type="checkbox" />
+        <label class="menu-button-container" for="menu-toggle">
+          <div class="menu-button"></div>
+        </label>
+
         {!showLoginBtn || Object.keys(user).length ? null : (
           <button className={`${styles.loginBtn}`}>
             <Link to="/loginreg">Sign Up | Login</Link>
           </button>
         )}
 
-      
-          <ul className={styles.mainNav}>
-            {(!showLoginBtn && Object.keys(user).length === 0) ||
-            (showLoginBtn && Object.keys(user).length) ? (
-              <span className={styles.leftSpan}>
-                <li>
-                  <Link to="/">Dashboard</Link>
-                </li>
-              </span>
-            ) : null}
-            {Object.keys(user).length ? (
-              <span className={styles.rightSpan}>
-                <li>
-                  <Link to="/create">Create Item</Link>
-                </li>
-                <li>
-                  <Link to={`/myItems/${user.userLoggedIn}`}>My Items</Link>
-                </li>
-                <li>
-                  <Link to="/" onClick={logout}>
-                    Log Out
-                  </Link>
-                </li>
-              </span>
-            ) : null}
-          </ul>
+        <ul className={`menu ${styles.mainNav}`}>
+          {(!showLoginBtn && Object.keys(user).length === 0) ||
+          (showLoginBtn && Object.keys(user).length) ? (
+            <span className={styles.leftSpan}>
+              <li>
+                <Link to="/">Dashboard</Link>
+              </li>
+            </span>
+          ) : null}
+          {Object.keys(user).length ? (
+            <span className={styles.rightSpan}>
+              <li>
+                <Link to="/create">Create Item</Link>
+              </li>
+              <li>
+                <Link to={`/myItems/${user.userLoggedIn}`}>My Items</Link>
+              </li>
+              <li>
+                <Link to="/" onClick={logout}>
+                  Log Out
+                </Link>
+              </li>
+            </span>
+          ) : null}
+        </ul>
       </div>
     </div>
   );
