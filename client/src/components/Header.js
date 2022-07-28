7097f85605530e024e27bdcf1ee9ddbcb8d756ce
Christina pilat <cpmoneyy@gmail.com>
2022-07-28 11:13:42 -0400

    updated myitemdisplay css & added lists to nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,64 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 
 import logo from "../images/logo.png";
 
 import styles from "../styles/header.module.css";
 
-
 const Header = (props) => {
   const { showLoginBtn, user, setUser } = props;
   const navigate = useNavigate();
-
+  const [showMenu, setShowMenu] = useState(false);
 
   const logout = () => {
-    axios.post("http://localhost:8000/api/users/logout",
-    {},
-    {
-      withCredentials: true
-    }
-    )
-    .then((res)=>{
-      console.log(res.data);
-      setUser({});
-      navigate('/');
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-  }
+    axios
+      .post(
+        "http://localhost:8000/api/users/logout",
+        {},
+        {
+          withCredentials: true,
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        setUser({});
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  // const showNav = () => {
+  //   showMenu ? "" : styles.hidden 
+  // }
 
   return (
-    <div className={`container ${styles.container}`}>
-      <div className={styles.navigation}>
-        <div className={styles.navLeft}>
-          <Link to="/">
-            <img className={styles.logo} src={logo} alt="logo" />
-          </Link>
-        </div>
-        <div className={styles.navRight}>
-          {!showLoginBtn || Object.keys(user).length ? null : (
-            <button className={`${styles.loginBtn}`}>
-              <Link to="/loginreg">Sign Up | Login</Link>
-            </button> 
-          )}
-          <span className={styles.mainNav}>
-            {(!showLoginBtn && Object.keys(user).length === 0) || (showLoginBtn && Object.keys(user).length) ? 
-            <span className={styles.leftSpan}>
-              <Link to="/">Dashboard</Link>
-            </span> : null }
-            {Object.keys(user).length ?
-            <span className={styles.rightSpan}> |
-              <Link to="/create">Create Item</Link>
-              <span> | </span>
-              <Link to={`/myItems/${user.userLoggedIn}`}>My Items</Link> | 
-              {/* ^^I will make sure this has the proper authentication on it */}
-              <Link to="/" onClick={logout}>Log Out</Link>
-            </span> : null}
-          </span>
-        </div>
+    <div className={styles.navigation}>
+      <div className={styles.navLeft}>
+        <Link to="/">
+          <img className={styles.logo} src={logo} alt="logo" />
+        </Link>
+      </div>
+      <div className={styles.navRight}>
+        {!showLoginBtn || Object.keys(user).length ? null : (
+          <button className={`${styles.loginBtn}`}>
+            <Link to="/loginreg">Sign Up | Login</Link>
+          </button>
+        )}
+
+      
+          <ul className={styles.mainNav}>
+            {(!showLoginBtn && Object.keys(user).length === 0) ||
+            (showLoginBtn && Object.keys(user).length) ? (
+              <span className={styles.leftSpan}>
+                <li>
+                  <Link to="/">Dashboard</Link>
+                </li>
+              </span>
+            ) : null}
+            {Object.keys(user).length ? (
+              <span className={styles.rightSpan}>
+                <li>
+                  <Link to="/create">Create Item</Link>
+                </li>
+                <li>
+                  <Link to={`/myItems/${user.userLoggedIn}`}>My Items</Link>
+                </li>
+                <li>
+                  <Link to="/" onClick={logout}>
+                    Log Out
+                  </Link>
+                </li>
+              </span>
+            ) : null}
+          </ul>
       </div>
     </div>
   );
